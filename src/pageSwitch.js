3e769eacecc4c946622777e0305a140a4693a68c
qiqiboy <imqiqiboy@gmail.com>
2014-10-25 15:03:57 +0800

    update

--- src/pageSwitch.js
+++ src/pageSwitch.js
@@ -24,8 +24,19 @@
         EVENT='PointerEvent' in ROOT ?
             "pointerdown pointermove pointerup pointercancel" :
             isTouch ? "touchstart touchmove touchend touchcancel" :
-            "mousedown mousemove mouseup";
-        
+            "mousedown mousemove mouseup",
+        transform=function(){
+            var divstyle=document.documentElement.style,
+                tests="transform webkitTransform mozTransform msTransform oTransform".split(" "),
+                prop;
+            while(prop=tests.shift()){
+                if(prop in divstyle){
+                    return prop;
+                }
+            }
+            return '';
+        }();
+        console.log(transform)
     function type(obj){
         if(obj==null){
             return String(obj);
@@ -88,8 +99,7 @@
     }
 
     function position(elem){
-        var ret={x:0,y:0},
-            rect=elem.getBoundingClientRect(),
+        var rect=elem.getBoundingClientRect(),
             parentRect=(elem.offsetParent||document.documentElement).getBoundingClientRect();
         return {x:rect.left-parentRect.left,y:rect.top-parentRect.top};
     }
@@ -130,7 +140,7 @@
         init:function(){
             var self=this;
             this.pages=children(this.container);
-            this.duration=800;
+            this.duration=600;
             this.direction=1;
             this.current=0;
             this.length=this.pages.length;
@@ -138,39 +148,66 @@
                 self.handleEvent(ev);
             });
             each(this.pages,function(page){
-                 page.style.display='none';
+                 page.style.cssText='position:absolute;top:0;left:0;width:100%;height:100%;display:none;';
+                 page.percent=0;
             });
             this.resize();
         },
         setPos:function(elem,value){
-            var transform,self=this;
-            each("transform webkitTransform mozTransform msTransform".split(" "),function(prop){
-                if(!transform && (prop in elem.style)){
-                    elem.style[prop]=transform='translate'+['X','Y'][self.direction]+'('+value+') translateZ(0)';
-                }
-            });
-            if(!transform){
-                elem.style[['left','top'][this.direction]]=value;
+            if(transform){
+                elem.style[transform]='translate'+['X','Y'][this.direction]+'('+value+'%)'+(transform.replace('Transform','Perspective') in elem.style?' translateZ(0)':'');
+            }else{
+                elem.style[['left','top'][this.direction]]=value+'%';
             }
+        },
+        onin:function(elem,percent){
+            this.setPos(elem,elem.percent=percent);
+        },
+        onout:function(elem,percent){
+            this.setPos(elem,elem.percent=percent>0?percent-100:100+percent);
         },
         resize:function(){
             this.pages[this.current].style.display='block'; 
             this.height=this.container.offsetHeight;
             this.width=this.container.offsetWidth;
         },
-        slide:function(index,tpage){console.log(index)
-            var cpage;
+        slide:function(index,tpage){
+            var self=this,
+                dir=this.direction,
+                total=this[['width','height'][dir]],
+                duration=this.duration,
+                cpage,percent,stime=+new Date;
             index=Math.min(this.length-1,Math.max(0,index));
             cpage=this.pages[index];
             if(index!=this.current){
                 tpage=this.pages[this.current];
             }
-            
-            this.setPos(cpage,'0');
-            cpage.style.display='block';
-            if(tpage)tpage.style.display='none';
+
+            if(cpage.style.display=='none'){
+                cpage.style.display='block';
+                percent=index>this.current?100:-100;
+            }else{
+                percent=cpage.percent;
+            }
 
             this.current=index;
+
+            ani();
+
+            function ani(){
+                var s=Math.min(1,(+new Date-stime)/(duration*Math.abs(percent)/100)),
+                    cp=percent*(1-s);
+                self.onin(cpage,cp);
+                if(tpage){
+                    self.onout(tpage,cp);
+                }
+                if(s==1){
+                    if(tpage)tpage.style.display='none';
+                    self.timer=null;
+                }else{
+                    self.timer=nextFrame(ani);
+                }
+            }
         },
         handleEvent:function(oldEvent){
             var ev=filterEvent(oldEvent);
@@ -182,7 +219,7 @@
                     var nn=ev.target.nodeName.toLowerCase();
                     if(nn!='a' && nn!='img'){
                         this.rect=[ev.clientX,ev.clientY];
-                        this.pos=position(this.pages[this.current]);
+                        this.percent=this.pages[this.current].percent;
                         this.time=+new Date;
                     }
                     break;
@@ -195,27 +232,29 @@
                             dir=this.direction,
                             offset=rect[dir]-this.rect[dir],
                             cpage=this.pages[this.current],
-                            pos=this.pos[['x','y'][dir]],
+                            pos=this.percent*total,
                             total=this[['width','height'][dir]],
-                            tpage,_tpage,cpos
+                            tpage,_tpage,cpos,percent;
                         if(this.drag==null && this.rect.toString()!=rect.toString()){
                             this.drag=Math.abs(offset)>Math.abs(rect[1-dir]-this.rect[1-dir]);
+                            cancelFrame(this.timer);
                         }
                         if(this.drag){
-                            cpos=offset+pos;
-                            tpage=this.pages[this.current+(cpos>0?-1:1)];
-                            _tpage=this.pages[this.current+(cpos>0?1:-1)];
-                            this.setPos(cpage,cpos/total/(tpage?1:3)*100+'%');
+                            percent=this.percent+offset/total*100;
+                            tpage=this.pages[this.current+(percent>0?-1:1)];
+                            _tpage=this.pages[this.current+(percent>0?1:-1)];
+                            this.onin(cpage,percent/(tpage?1:3));
+                            //this.setPos(cpage,(cpage.percent=percent/(tpage?1:3))+'%');
                             if(tpage){
                                 tpage.style.display='block';
-                                this.setPos(tpage,(cpos>0?cpos/total-1:1+cpos/total)*100+'%');
+                                //this.setPos(tpage,(tpage.percnet=(cpos>0?percent-100:100+percent))+'%');
+                                this.onout(tpage,percent);
                             }
                             if(_tpage){
                                 _tpage.style.display='none';
                             }
                             this._offset=offset;
-                            this._cpos=cpos;
-                            this._total=total;
+                            this._cpage=cpage;
                             this._tpage=tpage;
                             ev.preventDefault();
                         }
@@ -232,13 +271,13 @@
                             var index=this.current;
                             if(+new Date-this.time<250 && Math.abs(this._offset)>30){
                                 index+=this._offset>0?-1:1;
-                            }else if(Math.abs(this._cpos)/this._total>.5){
-                                index+=this._cpos>0?-1:1;
+                            }else if(Math.abs(this._cpage.percent)>50){
+                                index+=this._cpage.percent>0?-1:1;
                             }
                             this.slide(index,this._tpage);
                             ev.preventDefault();
                         }
-                        this.rect=this.drag=this.time=this._offset=this._cpos=this._total=this._tpage=null;
+                        this.rect=this.drag=this.time=this._offset=this.percent=this._cpage=this._tpage=null;
                     }
                     break;
 
@@ -266,3 +305,6 @@
 });
 
 a=new pageSwitch('pages');
+//a.setPos=function(elem,value){
+  //  elem.style.webkitTransform='translateY('+value+'%) scale('+(value>0?100-value:100+value)/100+')'
+//}
