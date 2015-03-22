d18df9aec7f1bdce6de2af3c7d8f38b4f790fbf1
qiqiboy <imqiqiboy@gmail.com>
2015-03-22 18:59:38 +0800

    update

--- src/pageSwitch.js
+++ src/pageSwitch.js
@@ -64,7 +64,11 @@
         class2type={},
         EASE={
             linear:function(t,b,c,d){ return c*t/d + b; },
-            ease:function(t,b,c,d){ return -c * ((t=t/d-1)*t*t*t - 1) + b; }
+            ease:function(t,b,c,d){ return -c * ((t=t/d-1)*t*t*t - 1) + b; },
+            'ease-in':function(t,b,c,d){ return c*(t/=d)*t*t + b; },
+            'ease-out':function(t,b,c,d){ return c*((t=t/d-1)*t*t + 1) + b; },
+            'ease-in-out':function(t,b,c,d){ if ((t/=d/2) < 1) return c/2*t*t*t + b; return c/2*((t-=2)*t*t + 2) + b; },
+            bounce:function(t,b,c,d){ if ((t/=d) < (1/2.75)) { return c*(7.5625*t*t) + b; } else if (t < (2/2.75)) { return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b; } else if (t < (2.5/2.75)) { return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b; } else { return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b; } }
         },
         TRANSITION={
             /* 更改切换效果
