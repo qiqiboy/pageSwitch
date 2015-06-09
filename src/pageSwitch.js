1a121c5f9f83f3016c58507b5394b8dae8c941e9
qiqiboy <imqiqiboy@gmail.com>
2015-06-09 11:42:37 +0800

    update

--- src/pageSwitch.js
+++ src/pageSwitch.js
@@ -822,7 +822,7 @@
                             _rect=this.rect,
                             offset=rect[dir]-_rect[dir],
                             cpage=this.pages[cIndex],
-                            total=this.offsetParent['offset'+['Width','Height'][dir]],
+                            total=this.offsetParent['client'+['Width','Height'][dir]],
                             tIndex,percent;
                         if(this.drag==null && _rect.toString()!=rect.toString()){
                             this.drag=Math.abs(offset)>=Math.abs(rect[1-dir]-_rect[1-dir]);
